6904fba02d926e65706549dc94f52fb96fd948c6
Williams Mata <williams.rm99@gmail.com>
2023-03-06 20:14:45 -0400

    issuesApp first section done

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,35 @@
+import { FC } from "react";
 
-export const LabelPicker = () => {
-  return (
-    <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
-    </div>
-  )
+import { useLabels } from "../hooks/useLabels";
+import { LoadingIcon } from "../../shared/components/LoadingIcon";
+
+interface Props {
+  selectedLabels: string[];
+  onChange: (labelName: string) => void;
 }
+
+export const LabelPicker: FC<Props> = ({ selectedLabels, onChange }) => {
+  const { labelsQuery } = useLabels();
+
+  if (labelsQuery.isLoading) return <LoadingIcon />;
+
+  return (
+    <>
+      {labelsQuery.data?.map((label) => (
+        <span
+          onClick={() => onChange(label.name)}
+          key={label.id}
+          className={`badge rounded-pill m-1 label-picker ${
+            selectedLabels.includes(label.name) ? "label-active" : ""
+          }`}
+          style={{
+            border: `1px solid #${label.color}`,
+            color: `#${label.color}`,
+          }}
+        >
+          {label.name}
+        </span>
+      ))}
+    </>
+  );
+};
